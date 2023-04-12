0024d96f37e9697d5e8ce011aa584985a5362443
Mariuszsu <47736300+Mariuszsu@users.noreply.github.com>
2023-04-12 20:29:05 +0200

    frontend
    add frontend

---
+++ frontend/app/src/_nav.js
@@ -0,0 +1,132 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+import { cilPuzzle, cilSpeedometer, cilStar } from '@coreui/icons'
+import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+
+const _nav = [
+  {
+    component: CNavItem,
+    name: 'Menu główne',
+    to: '/dashboard',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavTitle,
+    name: 'Components',
+  },
+  {
+    component: CNavGroup,
+    name: 'Base',
+    to: '/base',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Accordion',
+        to: '/base/accordion',
+      },
+      {
+        component: CNavItem,
+        name: 'Breadcrumb',
+        to: '/base/breadcrumbs',
+      },
+      {
+        component: CNavItem,
+        name: 'Cards',
+        to: '/base/cards',
+      },
+      {
+        component: CNavItem,
+        name: 'Carousel',
+        to: '/base/carousels',
+      },
+      {
+        component: CNavItem,
+        name: 'Collapse',
+        to: '/base/collapses',
+      },
+      {
+        component: CNavItem,
+        name: 'List group',
+        to: '/base/list-groups',
+      },
+      {
+        component: CNavItem,
+        name: 'Navs & Tabs',
+        to: '/base/navs',
+      },
+      {
+        component: CNavItem,
+        name: 'Pagination',
+        to: '/base/paginations',
+      },
+      {
+        component: CNavItem,
+        name: 'Placeholders',
+        to: '/base/placeholders',
+      },
+      {
+        component: CNavItem,
+        name: 'Popovers',
+        to: '/base/popovers',
+      },
+      {
+        component: CNavItem,
+        name: 'Progress',
+        to: '/base/progress',
+      },
+      {
+        component: CNavItem,
+        name: 'Spinners',
+        to: '/base/spinners',
+      },
+      {
+        component: CNavItem,
+        name: 'Tables',
+        to: '/base/tables',
+      },
+      {
+        component: CNavItem,
+        name: 'Tooltips',
+        to: '/base/tooltips',
+      },
+    ],
+  },
+  {
+    component: CNavGroup,
+    name: 'Icons',
+    icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'CoreUI Free',
+        to: '/icons/coreui-icons',
+        badge: {
+          color: 'success',
+          text: 'NEW',
+        },
+      },
+      {
+        component: CNavItem,
+        name: 'CoreUI Flags',
+        to: '/icons/flags',
+      },
+      {
+        component: CNavItem,
+        name: 'CoreUI Brands',
+        to: '/icons/brands',
+      },
+    ],
+  },
+  {
+    component: CNavTitle,
+    name: 'Extras',
+  },
+  {
+    component: CNavItem,
+    name: 'Wyloguj',
+    to: '/login',
+  },
+]
+
+export default _nav
